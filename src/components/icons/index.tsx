ed7fcb1979f790247cf0e835fed3051e323081f6
Erikbernard <erikbrsilva@gmail.com>
2024-03-17 13:33:40 -0300

    feat: icons component

---
+++ src/components/icons/index.tsx
@@ -0,0 +1,20 @@
+
+// eslint-disable-next-line @typescript-eslint/ban-ts-comment
+// @ts-nocheck
+import { ReactComponent as Sescomp } from "../../assets/secomp.svg";
+
+
+import { ReactComponent as Facebook } from "../../assets/icon_facebook.svg";
+import { ReactComponent as Instagram } from "../../assets/icon_instagram.svg";
+import { ReactComponent as linkedin } from "../../assets/icon_linkedin.svg";
+import { ReactComponent as Youtube } from "../../assets/icon_youtube.svg";
+import { ReactComponent as Arrow } from "../../assets/icon/ARROW.svg";
+
+export const Icon = {
+  Sescomp,
+  Facebook,
+  Instagram,
+  Youtube,
+  linkedin,
+  Arrow,
+};
