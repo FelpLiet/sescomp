1aa585dc7b638b47da7479e59999edaaa207bf90
Felipe Nogueira <felipe.leite23@gmail.com>
2024-11-04 15:04:29 -0300

    refactor: Update speaker carousel resposiveness

--- src/components/Speakers/index.tsx
+++ src/components/Speakers/index.tsx
@@ -1,29 +1,29 @@
+import { useEffect, useState } from "react";
 import styles from "./styles.module.scss";
 import { SpeakerItem } from "./speakerItem";
-import jessica from "../../assets/palestrante/jessica.png";
-import amalia from "../../assets/palestrante/amalia.png";
-import antonio from "../../assets/palestrante/antonio.png";
-import bruno from "../../assets/palestrante/bruno.png";
-import francisco from "../../assets/palestrante/francisco.png";
-import gabriela from "../../assets/palestrante/gabriela.png";
-import julissy from "../../assets/palestrante/julissy.png";
-import michelle from "../../assets/palestrante/michelle.png";
-import nayara from "../../assets/palestrante/nayara.png";
 import Carousel from "../Carousel";
-import { useEffect, useState } from "react";
 import { Button } from "../Button";
 import { Icon } from "../icons";
+import alessandro from "../../assets/palestrante/alessandro.png";
+import gabriela from "../../assets/palestrante/gabriela.png";
+import izequiel from "../../assets/palestrante/izequiel.png";
+import nina from "../../assets/palestrante/nina.png";
+import paulina from "../../assets/palestrante/paulina.png";
+import sandro from "../../assets/palestrante/sandro.png";
+import silveira from "../../assets/palestrante/silveira.png";
+import thales from "../../assets/palestrante/thales.png";
+import yago from "../../assets/palestrante/yago.png";
 
 const speakersJson = [
-    { name: "Jessica Neiva", image: jessica },
-    { name: "Amália Beatriz", image: amalia },
-    { name: "Antonio W.", image: antonio },
-    { name: "Bruno Lessa", image: bruno },
-    { name: "Francisco R.", image: francisco },
+    { name: "Paulina Freitas", image: paulina },
+    { name: "Silveira Neto", image: silveira },
     { name: "Gabriela Q.", image: gabriela },
-    { name: "Julissy Bezerra", image: julissy },
-    { name: "Michelle A.", image: michelle },
-    { name: "Nayara V.", image: nayara },
+    { name: "Sandro Costa", image: sandro },
+    { name: "Alessandro F.", image: alessandro },
+    { name: "Yago Marques", image: yago },
+    { name: "Izequiel Pereira", image: izequiel },
+    { name: "Nina Talks", image: nina },
+    { name: "Thales Wylmar", image: thales },
 ];
 
 export function Speakers() {
@@ -50,7 +50,7 @@ export function Speakers() {
         };
 
         handleResize();
-        
+
         window.addEventListener("resize", handleResize);
         return () => window.removeEventListener("resize", handleResize);
     }, []);
@@ -60,7 +60,11 @@ export function Speakers() {
             <h1>Palestrantes</h1>
             <Carousel visibleItems={visibleItems}>
                 {speakersJson.map((speaker, index) => (
-                    <SpeakerItem key={index} name={speaker.name} image={speaker.image} />
+                    <SpeakerItem
+                        key={index}
+                        name={speaker.name}
+                        image={speaker.image}
+                    />
                 ))}
             </Carousel>
             <span style={{ marginTop: "24px" }}>
