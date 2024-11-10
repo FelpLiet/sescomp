4999e9ed121d5fbf072bc7d95f930548f3b4cea1
Felipe Nogueira <felipe.leite23@gmail.com>
2024-11-10 11:12:13 -0300

    update shop itens

--- src/data/products.ts
+++ src/data/products.ts
@@ -1,69 +1,18 @@
 import copoBeijos from "../assets/shop/copoBeijos.png";
 import copoBeakpoint from "../assets/shop/copoBreakpoints.png";
-import copo from "../assets/shop/copo.png";
-import copoPretoBranco from "../assets/shop/copoPretoBranco.png";
 import copoReinvente from "../assets/shop/copoReinvente.png";
-import copoSescomp from "../assets/shop/copoSescomp.png";
+import copoCircular from "../assets/shop/copoCircular.png";
 import canecaBeijos from "../assets/shop/canecaBeijos.png";
-import canecaBreakpoints from "../assets/shop/canecaBreakpoints.png";
 import canecaWhile from "../assets/shop/canecaWhile.png";
-import cadernetaBeijos from "../assets/shop/cadernetaBeijos.png";
-import cadernetaWhile from "../assets/shop/cadernetaWhile.png";
-import cadernetaAprendizado from "../assets/shop/cadernetaAprendizado.png";
+import canecaCircular from "../assets/shop/canecaCircular.png";
 import cadernetaIdeas from "../assets/shop/cadernetaIdeas.png";
-import camisaBrancaFrente from "../assets/shop/camisaBrancaFrente.png";
-import camisaBrancaVerso from "../assets/shop/camisaBrancaVerso.png";
-import camisaRoxaFrente from "../assets/shop/camisaRoxaFrente.png";
-import camisaRoxaVerso from "../assets/shop/camisaRoxaVerso.png";
 
 export const produtos = [
     {
-        nome: "Copo Beijos BUGS e Poesia",
-        preco: 12,
-        categoria: "Copos",
-        imgFront: copoBeijos,
-    },
-    {
-        nome: "Copo </>Breakpoints Coffee Breaks",
-        preco: 12,
-        categoria: "Copos",
-        imgFront: copoBeakpoint,
-    },
-    {
-        nome: "Copo SESCOMP Circular", 
-        preco: 12,
-        categoria: "Copos",
-        imgFront: copo,
-    },
-    {
-        nome: "Copo Preto e Branco",
-        preco: 12,
-        categoria: "Copos",
-        imgFront: copoPretoBranco,
-    },
-    {
-        nome: "Copo Reinvente",
-        preco: 12,
-        categoria: "Copos",
-        imgFront: copoReinvente,
-    },
-    {
-        nome: "Copo Sescomp",
-        preco: 12,
-        categoria: "Copos",
-        imgFront: copoSescomp,
-    },
-    {
-        nome: "Caneca Beijos BUGS e Poesia",
+        nome: "Caneca Circular",
         preco: 40,
         categoria: "Canecas",
-        imgFront: canecaBeijos,
-    },
-    {
-        nome: "Caneca Breakpoints Coffee Breaks",
-        preco: 40,
-        categoria: "Canecas",
-        imgFront: canecaBreakpoints,
+        imgFront: canecaCircular,
     },
     {
         nome: "Caneca While",
@@ -72,41 +21,39 @@ export const produtos = [
         imgFront: canecaWhile,
     },
     {
-        nome: "Caderneta Beijos BUGS e Poesia",
+        nome: "Caneca Beijos BUGS e Poesia",
         preco: 40,
-        categoria: "Cadernetas",
-        imgFront: cadernetaBeijos,
+        categoria: "Canecas",
+        imgFront: canecaBeijos,
     },
     {
-        nome: "Caderneta While",
-        preco: 0,
-        categoria: "Cadernetas",
-        imgFront: cadernetaWhile,
+        nome: "Copo Sescomp",
+        preco: 12,
+        categoria: "Copos",
+        imgFront: copoCircular,
     },
     {
-        nome: "Caderneta Aprendizado",
-        preco: 0,
-        categoria: "Cadernetas",
-        imgFront: cadernetaAprendizado,
+        nome: "Copo </>Breakpoints Coffee Breaks",
+        preco: 12,
+        categoria: "Copos",
+        imgFront: copoBeakpoint,
+    },
+    {
+        nome: "Copo Beijos BUGS e Poesia",
+        preco: 12,
+        categoria: "Copos",
+        imgFront: copoBeijos,
+    },
+    {
+        nome: "Copo Reinvente",
+        preco: 12,
+        categoria: "Copos",
+        imgFront: copoReinvente,
     },
     {
         nome: "Caderneta Ideas",
         preco: 0,
         categoria: "Cadernetas",
         imgFront: cadernetaIdeas,
-    },
-    {
-        nome: "Camisa Branca",
-        preco: 0,
-        categoria: "Camisas",
-        imgFront: camisaBrancaFrente,
-        imgBack: camisaBrancaVerso,
-    },
-    {
-        nome: "Camisa Roxa",
-        preco: 0,
-        categoria: "Camisas",
-        imgFront: camisaRoxaFrente,
-        imgBack: camisaRoxaVerso,
     },
 ];
